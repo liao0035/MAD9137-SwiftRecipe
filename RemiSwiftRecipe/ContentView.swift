d4aa650eeedafaadec2dcdfc81d3253107ca4afd
liao0035 <liao0035@algonquinlive.com>
2025-10-19 18:58:09 -0400

    Initial Commit

---
+++ RemiSwiftRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemiSwiftRecipe
+//
+//  Created by Remi Liao on 2025/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemiSwiftRecipe/RemiSwiftRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemiSwiftRecipeApp.swift
+//  RemiSwiftRecipe
+//
+//  Created by Remi Liao on 2025/10/19.
+//
+
+import SwiftUI
+
+@main
+struct RemiSwiftRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
